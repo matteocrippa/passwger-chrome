3b2c65c572f8995c2cd10bf65010c817106ff7e6
Matteo Crippa <matteo.crippa@gmail.com>
2015-01-31 22:02:50 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+/*
 function getCurrentTabUrl(callback) {
 
   chrome.tabs.query({
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 });
-
+*/
 
 function renderHost(host) {
   document.getElementById('host').value = host
