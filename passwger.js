0a69af0633ad264ecada39ca8faa25f34b1e255c
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-31 10:59:28 +0100

    new structure with unlock system

--- passwger.js
+++ passwger.js
@@ -1,21 +1,21 @@
 (function (){
   angular
     .module('passwger', [])
-  
+
   function passwgerController ($log, $window, $http) {
-  
+
     var vm = this
-    
+
     vm.lock = true
-    
+
     vm.checkPassword = function (){
       if(vm.lockedPassword.length == 0){
         alert('Please enter your unlock password')
         return
       }
-      
+
       $http
-        .post('http://localhost:12358/getPassword', { 
+        .post('http://localhost:12358/getPassword', {
           pwd: vm.lockedPassword
         })
         .success(function (data, status, headers, config) {
@@ -28,12 +28,12 @@
         .error(function (data, status, headers, config) {
           alert('Error, please open Passwger app')
         })
-      
+
     }
-  
+
   }
-  
+
   angular
     .module('passwger')
     .controller('PasswgerController', passwgerController)
-)()
+})()
