7f3bbbfc835c7a522cecb338c2200b4b16845c9d
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-31 21:26:14 +0100

    test

--- popup.js
+++ popup.js
@@ -2,12 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- **/
 function getCurrentTabUrl(callback) {
 
   chrome.tabs.query({
@@ -26,56 +20,16 @@ function getCurrentTabUrl(callback) {
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
 
-    getPasswordUrl(url, function(pwds) {
+    var link = document.createElement('a')
+    link.setAttribute('href', url)
 
-      renderHost(pwds)
+    var domain = link.hostname
 
-    }, function(errorMessage) {
-      alert('Cannot display image. ' + errorMessage);
-    });
+    renderHost(domain)
+
   });
 });
 
-
-function getPasswordUrl(url, callback, errorCallback) {
-
-  var link = document.createElement('a')
-  link.setAttribute('href', url)
-
-  var domain = link.hostname
-
-  renderHost('Host: ' + domain)
-
-  var x = new XMLHttpRequest()
-
-  x.open('POST', 'http://localhost:12358/getPassword', true)
-  x.responseType = 'json'
-  x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-
-  x.onload = function() {
-    var response = x.response
-
-    if (!response) {
-      errorCallback('No response')
-      return
-    }
-
-    if (response.error) {
-      errorCallback(response.error)
-      return
-    }
-
-    var results = response.pwds
-
-    callback(results)
-  }
-
-  x.onerror = function() {
-    errorCallback('Network error')
-  }
-
-  x.send('pwd=test')
-}
 
 function renderHost(host) {
   document.getElementById('host').value = host
