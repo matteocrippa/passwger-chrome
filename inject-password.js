15ba4c0657ac3e16b83d77aa67272876866fe74d
Matteo Crippa <matteo.crippa@gmail.com>
2015-02-04 20:40:00 +0100

    removed inject file

--- inject-password.js
+++
@@ -1,9 +0,0 @@
-var pArr = [];
-var pInputs = document.getElementByTagName("input");
-for (var i=pInputs.length; i>0; i--){\
-  if(pInputs[i].type.toLowerCase() === "password"){
-    pInputs[i].value = passwgerUser;
-    pInputs[i-1].value = passwgerPassw;
-    return;
-  }
-}
